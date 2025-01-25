f83b610a40ccaaacc0dc7a4e15f188b47325b7f2
Gabe Field <gabriel.field@uqconnect.edu.au>
2025-01-25 17:53:37 +1000

    shitpost

--- Notes/Chapter6/Sec5Universes.lean
+++ Notes/Chapter6/Sec5Universes.lean
@@ -1,3 +1,3 @@
-/- @file Notes/Chapter6/Sec4Alternatives.lean
- - @src https://lean-lang.org/functional_programming_in_lean/functor-applicative-monad/alternative.html
+/- @file Notes/Chapter6/Sec5Universes.lean
+ - @src https://lean-lang.org/functional_programming_in_lean/functor-applicative-monad/universes.html
  -/
